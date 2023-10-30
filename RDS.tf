f7b0f09671d6f5c82d2a2ba7faf2cd9c719dcbaa
sawantvarsha <87065915+sawantvarsha@users.noreply.github.com>
2023-10-30 17:47:00 +0530

    Update RDS.tf

--- RDS.tf
+++ RDS.tf
@@ -1,10 +1,10 @@
-resource "aws_db_instance" "example" {
+resource "aws_db_instance" "rds" {
   allocated_storage    = 20
   storage_type         = "gp2"
   engine               = "mysql"
   engine_version       = "5.7"
   instance_class       = "db.t2.micro"
-  name                 = "example-db"
+  name                 = "RDSDB"
   username             = "admin"
   password             = "password"
   parameter_group_name = "default.mysql5.7"
