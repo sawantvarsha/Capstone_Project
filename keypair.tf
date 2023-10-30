3f3d87282b440fa937065ac07317e7c0d56b7149
sawantvarsha <87065915+sawantvarsha@users.noreply.github.com>
2023-10-30 15:58:58 +0530

    Update keypair.tf

--- keypair.tf
+++ keypair.tf
@@ -1,12 +1,15 @@
-resource "aws_key_pair" "tf-key-pair" {
+Creates a PEM (and OpenSSH) formatted private key
+resource "aws_key_pair" "tf-key-pair" { 
 key_name = "tf-key-pair"
 public_key = tls_private_key.rsa.public_key_openssh
 }
+#Creates a PEM (and OpenSSH) formatted private key
 resource "tls_private_key" "rsa" {
 algorithm = "RSA"
 rsa_bits  = 4096
 }
-resource "local_file" "tf-key" {
+#create local file
+resource "local_file" "tf-key" { 
 content  = tls_private_key.rsa.private_key_pem
 filename = "tf-key-pair"
 }
